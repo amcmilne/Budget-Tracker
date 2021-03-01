ba33cd313b9c4b3495e56f7ce4a16e41d43e3e22
amcmilne <amcmilne@gmail.com>
2021-03-01 14:19:51 -0500

    add db.js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Budget-Tracker
 For this project, I will add functionality to our existing Budget Tracker application to allow for offline access and functionality.  The user will be able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online. 
+
+## Tech Required: 
+
+## Screenshots:
+
+## View my work: 
+
+## License:
+* MIT
---
+++ public/db.js
@@ -0,0 +1,70 @@
+//let db;
+const pendingObjectStoreName = "pending";
+// create a new db request for a "budget" database.
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+   // create object store called "pending" and set autoIncrement to true
+  const db = target.result;
+  if (!db.objectStoreNames.contains(pendingObjectStoreName)) {
+    db.createObjectStore(pendingObjectStoreName, { autoIncrement: true });
+}
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = ({ target }) => {
+  console.log("DOH!" + target.errorCode);
+};
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+
+          // access your pending object store
+          const store = transaction.objectStore("pending");
+
+          // clear all items in your store
+          store.clear();
+        });
+    }
+  };
+}
+function saveRecord(record) {
+// create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+   // access your pending object store
+   const store = transaction.objectStore("pending");
+
+   // add record to your store with add method.
+   store.add(record);
+ }
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
