be76605b00371f6f958819c9ff0456ba3bd9fee1
amcmilne <amcmilne@gmail.com>
2021-03-01 21:08:17 -0500

    fixed connection bug AGAIN

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const connectDB = require ("./connection.env");
 
-const PORT = 3000;
+connectDB();
+
+const PORT = process.env.URI || 3000;
 
 const app = express();
 
@@ -15,10 +18,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
